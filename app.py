d9a0c7e49a3286fea24bd165034afdb9a471bc54
João Cleiton <81659000+JoaoCleiton12@users.noreply.github.com>
2025-08-04 16:58:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_jwt_extended import create_access_token, jwt_required, JWTManager, ge
 app = Flask(__name__)
 
 # Configuração correta do banco de dados
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://iot_auth_fooy_user:ans9RyDcRMvuLKWMqJ37oidGtELd7sdz@dpg-d28gq53uibrs73atd3cg-a.oregon-postgres.render.com/iot_auth_fooy")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://iot_auth_fooy_user:ans9RyDcRMvuLKWMqJ37oidGtELd7sdz@dpg-d28gq53uibrs73atd3cg-a/iot_auth_fooy")
 
 def get_db_connection():
     try:
@@ -119,4 +119,5 @@ def get_broker_info():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
 
